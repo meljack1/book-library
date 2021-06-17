b49c35f09761bd3bccb117348959cb0c17bc50c4
Mel Jack <melissa_jack@hotmail.co.uk>
2021-06-17 16:57:52 +0100

    Add function to add book to array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,24 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
     this.info = function() {
         if(read) {
             return `${title} by ${author}, ${pages} pages, read`
         } else {
             return `${title} by ${author}, ${pages} pages, not read yet`
         }
-    }
+    };
 }
 
-const OT1 = new Book("Toaru Majutsu no Index Volume 1", "Kamachi Kazuma", 312, true);
+function addBookToLibrary() {
+    let title = prompt("What is the book's title?");
+    let author = prompt("What is the book's author?");
+    let pages = prompt("How many pages are in the book?");
+    let read = confirm("Have you read the book?")
+    myLibrary.push(new Book(title, author, pages, read));
+}
 
-console.log(OT1.info());
